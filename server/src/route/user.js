84262ca5f26b120904a59c1f7c8f0b2331cbe466
Venkataswamy M <swamy8533@gmail.com>
2023-03-15 22:14:34 -0700

    bcyrptjs

--- server/src/route/user.js
+++ server/src/route/user.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const UserModel = require('../models/User.js');
 // const Router = require('express');
 
